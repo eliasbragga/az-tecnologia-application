02731939a2f0ebc6a8bded22c37fd2ab0f48c62f
Elias notebook <eliasbragga44@gmail.com>
2023-04-14 11:53:31 -0300

    feat:adjusting-variable-names-test-login

--- src/views/login/__test__/login.spec.js
+++ src/views/login/__test__/login.spec.js
@@ -17,24 +17,24 @@ describe("Login.vue - Unit", () => {
 
   it("should return the set value ", async () => {
     const email = "eve.holt@reqres.in";
-    const senha = "eve.holt@reqres.in";
+    const password = "eve.holt@reqres.in";
     await wrapper.find("[data-jest='input_user_email']").setValue(email);
-    await wrapper.find("[data-jest='input_user_password']").setValue(senha);
+    await wrapper.find("[data-jest='input_user_password']").setValue(password);
     await wrapper.vm.$nextTick();
 
     expect(wrapper.vm.form.inputBind).toBe(email);
-    expect(wrapper.vm.form.passwordBind).toBe(senha);
+    expect(wrapper.vm.form.passwordBind).toBe(password);
   });
 
   it("should set the values of data ​​to vuex  ", async () => {
     const email = "eve.holt@reqres.in";
-    const senha = "123456";
+    const password = "123456";
     await wrapper.find("[data-jest='input_user_email']").setValue(email);
-    await wrapper.find("[data-jest='input_user_password']").setValue(senha);
+    await wrapper.find("[data-jest='input_user_password']").setValue(password);
     await wrapper.find("[data-jest='button_user_login']").trigger("click");
     await wrapper.vm.$nextTick();
 
     expect(wrapper.vm.user).toBe(email);
-    expect(wrapper.vm.password).toBe(senha);
+    expect(wrapper.vm.password).toBe(password);
   });
 });
