90536516c4ebfc0d534d01baee53546ab8f25cd0
Elias notebook <eliasbragga44@gmail.com>
2023-04-14 13:41:55 -0300

    feat:adjusting-register-test

--- src/views/register/__test__/register.spec.js
+++ src/views/register/__test__/register.spec.js
@@ -2,7 +2,6 @@ import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Register from "../index.vue";
 import Vuex from "vuex";
 import loginVuex from "@/store/login";
-import axios from "axios";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -16,40 +15,18 @@ describe("Register.vue - Unit", () => {
     wrapper = shallowMount(Register, { store, localVue });
   });
 
-  it("should return the set value ", async () => {
+  it("should define value of data variables ", async () => {
     const email = "eve.holt@reqres.in";
     const password = "123456";
-    await wrapper.find("[data-jest='input_user_register_email']").setValue(email);
-    await wrapper.find("[data-jest='input_user_register_password']").setValue(password);
+    await wrapper
+      .find("[data-jest='input_user_register_email']")
+      .setValue(email);
+    await wrapper
+      .find("[data-jest='input_user_register_password']")
+      .setValue(password);
     await wrapper.vm.$nextTick();
 
     expect(wrapper.vm.form.inputBind).toBe(email);
     expect(wrapper.vm.form.passwordBind).toBe(password);
-  });
-
-  it("should set the values of data ​​to vuex", async () => {
-    // const token = 'QpwL5tke4Pnpja7X4'
-    // axios.interceptors.request.use(() => {
-    //     return {
-    //         token,
-    //     };
-    //   });
-
-
-    // const email = "eve.holt@reqres.in";
-    // const password = "123456";
-    // await wrapper
-    //   .find("[data-jest='input_user_register_email']")
-    //   .setValue(email);
-    // await wrapper
-    //   .find("[data-jest='input_user_register_password']")
-    //   .setValue(password);
-    // await wrapper.find("[data-jest='button_user_register']").trigger("click");
-    // await wrapper.vm.handleClick()
-    // await wrapper.vm.$nextTick();
-    // expect(wrapper.vm.token).toBe()
-
-    // expect(wrapper.vm.user).toBe(email);
-    // expect(wrapper.vm.password).toBe(password);
   });
 });
