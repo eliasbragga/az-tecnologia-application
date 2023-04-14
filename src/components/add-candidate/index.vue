df5ffcb20785593db941d88c577197e358a7fb5a
Elias notebook <eliasbragga44@gmail.com>
2023-04-14 03:16:10 -0300

    feat:added-recent-candidates-component

---
+++ src/components/add-candidate/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="table_container">
+    <ActionUser @messageToast="messageToastHandle" :edit="false" v-show="false" />
+    <MessageAlert
+      :showMessage="showMessage"
+      :message="messageToast"
+      :successReq="successReq"
+      @closeToastMessage="closeToastMessage"
+    />
+    <div class="table_empty_container">
+      <img src="@/assets/Vector (5).png" alt="icone no centro da tela vazia" />
+      <p>
+        Você nao possui nenhum candidato adicionado recentemente, clique no
+        botão
+        <span class="button_add_span">Adicionar</span> para começar.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import MessageAlert from "@/components/message-alert/index.vue";
+import ActionUser from "@/components/popups/action-candidate.vue";
+import MyMixin from "@/mixins/MyMixin.js";
+export default {
+  name: "AddCandidate",
+  mixins:[MyMixin],
+  components: { ActionUser, MessageAlert },
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.table_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 97%;
+  height: 100vh;
+  background: #ffffff;
+  overflow: hidden;
+  overflow-x: hidden;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.05);
+  border-radius: 6px;
+  margin: 0.5rem auto;
+  padding: 1rem;
+}
+
+.table_empty_container {
+  flex-direction: column;
+  width: 35%;
+  margin: auto;
+}
+.table_empty_container p {
+  font-family: "Open Sans";
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+  color: #626262;
+}
+.table_empty_container img {
+  margin-bottom: 0.5rem;
+}
+
+.button_add_span {
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 27px;
+  color: #0067a5;
+}
+
+@media screen and (min-width: 600px) and ( max-width: 900px)  {
+  .table_empty_container {
+    width: 60%;
+  }
+}
+@media screen and ( max-width: 600px)  {
+  .table_empty_container {
+    width: 90%;
+  }
+}
+</style>
